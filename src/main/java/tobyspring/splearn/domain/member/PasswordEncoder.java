f9fe1096193c4d2d1400a1169e9daf6788764c92
hyunil-cho <jackandjills@naver.com>
2025-06-26 22:59:56 +0900

    코드 초기화

--- src/main/java/tobyspring/splearn/domain/member/PasswordEncoder.java
+++
@@ -1,6 +0,0 @@
-package tobyspring.splearn.domain.member;
-
-public interface PasswordEncoder {
-    String encode(String password);
-    boolean matches(String password, String passwordHash);
-}
--- src/main/java/tobyspring/splearn/domain/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package tobyspring.splearn.domain;
-
-import org.springframework.lang.NonNullApi;
